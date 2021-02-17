5e6af9435f3b4bbc3f826131a37256ecc6c869bf
Antal József <76700822+antaljozsef@users.noreply.github.com>
2021-02-17 19:19:41 +0200

    1 Labor frissitve

--- Labor1/src/com/company/Main.java
+++ Labor1/src/com/company/Main.java
@@ -6,24 +6,59 @@ import java.sql.SQLOutput;
 
 public class Main {
 
+    public static double maxElement( double array[] ){
+        double max = Double.NEGATIVE_INFINITY;
+        for( int i=0; i<array.length; ++i ){
+            if( array[i] > max){
+                max = array[ i ];
+            }
+        }
+        return max;
+    }
+
     public static void main(String[] args) {
-	    // 1Feladat
+        // I Egyszeru main - 1Feladat
         System.out.println("Jozsi");
 
         // 2Feladat
         String nev = "Antal Jozsef";
-        for( int i=0; i<nev.length(); ++i){
+        for (int i = 0; i < nev.length(); ++i) {
             System.out.println(nev.charAt(i));
         }
+
+        System.out.printf("\n");
 
         // 3Feladat
         String szo = "ALMAFA";
 
-        for (int i=0; i<row; i++)
-        {
-            for (int i=0; i)
+        int l;
+        l = szo.length();
+
+        for (int i = 0; i < l; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.printf("%c", szo.charAt(j));
+            }
+            System.out.printf("%c\n", szo.charAt(i));
         }
 
+        System.out.printf("\n");
+
+        // 4feladat
+
+        String[] tomb;
+        String nev2 = "Kerekes Bálint Ádám József";
+        tomb = nev2.split(" ");
+
+        for (int i=0; i < tomb.length; i++) {
+            System.out.printf("%c",tomb[i].charAt(0));
+        }
+
+        System.out.printf("\n\n");
+
+        // II Fugvenyenk - 1feladat
+
+        double x[] ={7, 1, -3, 45, 9};
+        System.out.printf("MAX: %6.2f\n", maxElement( x ));
 
     }
 }
