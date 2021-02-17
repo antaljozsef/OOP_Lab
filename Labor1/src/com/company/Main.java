bfec74d3fa69dbc455f37184a8a1e2388d5b16c9
Antal József <76700822+antaljozsef@users.noreply.github.com>
2021-02-17 16:15:29 +0200

    1 Labor frissitve

--- Labor1/src/com/company/Main.java
+++ Labor1/src/com/company/Main.java
@@ -1,9 +1,29 @@
 package com.company;
 
+import javax.swing.*;
+import java.net.SocketTimeoutException;
+import java.sql.SQLOutput;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    // 1Feladat
         System.out.println("Jozsi");
+
+        // 2Feladat
+        String nev = "Antal Jozsef";
+        for( int i=0; i<nev.length(); ++i){
+            System.out.println(nev.charAt(i));
+        }
+
+        // 3Feladat
+        String szo = "ALMAFA";
+
+        for (int i=0; i<row; i++)
+        {
+            for (int i=0; i)
+        }
+
+
     }
 }
