8c613dc48f851688e1929687657f2d74ab2f6740
Antal József <76700822+antaljozsef@users.noreply.github.com>
2021-02-25 13:49:05 +0200

    Javítás

--- Labor2/Auto/src/auto/Auto.java
+++ Labor2/Auto/src/auto/Auto.java
@@ -4,24 +4,21 @@ public class Auto {
     private String color;
     private int speed;
 
-    public Auto(String color){
+    public Auto(String color) {
         this.color = color;
     }
 
-    public void setColor(String color){
+    public void setColor(String color) {
         this.color = color;
     }
 
-    public void setSpeed(int speed){
+    public void setSpeed(int speed) {
         this.speed = speed;
     }
 
     @Override
 
-    public String toString(){
+    public String toString() {
         return "Car color: " + this.color + ", Car speed: " + this.speed;
     }
 }
-
-
-
